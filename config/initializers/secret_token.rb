8ea20eb377f0194f2b079069e84991011316ceb5
Viktor <viktor.markevich@faceit.com.ua>
2012-10-29 10:33:15 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gladius::Application.config.secret_token = 'f40fdb33445674f2092ee67a7c9fd616a6156ee342f26f3ea6ff0e8200361f3f8014d94f137ae47a9ee9d398bda5edb89c3570baeb864490910a199e6a49da56'
