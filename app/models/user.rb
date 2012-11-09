36137fdc298ec01a76c9692f0e0555f18e2b9a9e
Viktor <viktor.markevich@faceit.com.ua>
2012-11-09 17:25:28 +0200

    added devise; views of devise; added login method create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :about, :address, :birthday, :city, :country, :first_name, :last_name, :level, :second_name, :sex, :status, :weight
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :about, :address, :birthday, :city, :country, :first_name, :last_name, :level, :second_name, :sex, :status, :weight, :login
 
   belongs_to :group
   belongs_to :list_registration
@@ -13,4 +21,16 @@ class User < ActiveRecord::Base
   has_many :duels, :as => :fighter
   has_many :comments
   has_many :posts
+
+  before_create :create_login
+
+  def create_login
+    email = self.email.split(/@/)
+    login_taken = User.where( :login => email[0]).first
+    unless login_taken
+      self.login = email[0]
+    else
+      self.login = self.email
+    end
+  end
 end
