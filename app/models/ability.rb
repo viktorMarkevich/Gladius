c28d26be253dff376f66417edf520eeb1303f7de
Viktor <viktor.markevich@faceit.com.ua>
2012-11-19 12:41:51 +0200

    condition
    сreated the conditions for the manager

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,19 +3,20 @@ class Ability
 
   def initialize(user)
     if user.role? :manager
-      #can :manage, School.where(:id => user.school_id)
-      #can :manage, User.where(:school_id => user.school_id)
-      can :manage, User
+      can :create,  School
+      can :update,  School.where(:id => user.school_id)
+      can :create,  User
+      can :update,  User.where(:school_id => user.school_id)
+      can :destroy, User.where(:school_id => user.school_id, :role => ["pupil", "moderator"])
     end
 
     if user.role? :moderator
-      can :create, School
-      can :destroy, User.where(:role => "pupil")
-      can :create, User
-      can :update, User.where(:role => "pupil")
-      cannot :update, User.where(:role => ["manager", "moderator"])
-      can :do_this, :for_moderator
-      cannot :create, :for_moderator
+      #can :create, School
+      #can :destroy, User.where(:role => "pupil")
+      #can :create, User
+      #can :update, User.where(:role => "pupil")
+      #cannot :update, User.where(:role => ["manager", "moderator"])
+      #can :do_this, :for_moderator
     end
 
     if user.role? :pupil
