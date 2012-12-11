deddd9a5c029a6d111925448318ef1df0d59ccde
Viktor <viktor.markevich@faceit.com.ua>
2012-12-11 11:48:14 +0200

    abilty
    added helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
+#encoding: utf-8
 module UsersHelper
+
+  def td_value(user)
+    if get_user_school_relation(user)
+      if (can? :update, user) || (user == current_user)
+        link_to "Редактировать", edit_user_path(user)
+      else
+        link_to "Пригласить", "#"
+      end
+    else
+      if (user == current_user)
+        link_to "Редактировать", edit_user_path(user)
+      else
+        link_to "Пригласить", "#"
+      end
+    end
+  end
+
 end
