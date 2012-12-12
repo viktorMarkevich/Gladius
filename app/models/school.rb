6bec4f04243e805c4f6056e0647b5122832de0eb
Viktor <viktor.markevich@faceit.com.ua>
2012-12-12 17:12:00 +0200

    added dependet to school

--- app/models/school.rb
+++ app/models/school.rb
@@ -7,7 +7,7 @@ class School < ActiveRecord::Base
   has_many :contact_info, :as => :info_for
   has_many :training_places
   has_many :honors, :as => :item
-  has_many :user_school_relations
+  has_many :user_school_relations, :dependent => :destroy
   has_many :user, :through => :user_school_relations
 
   accepts_nested_attributes_for :contact_info
