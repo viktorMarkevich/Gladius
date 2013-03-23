9f6f88da1716804692966e825b7522513300caaa
Viktor <markevich-viktor@mail.ru>
2013-03-23 15:38:03 +0200

    changed relation

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,5 @@
 class School < ActiveRecord::Base
-  attr_accessible :date_of_foundation, :info, :school_name, :status
+  attr_accessible :date_of_foundation, :info, :school_name, :status, :contact_info_attributes
 
   belongs_to :fighting_art
 
@@ -7,7 +7,7 @@ class School < ActiveRecord::Base
   has_many :training_places
   has_many :honors, :as => :item
   has_many :user_school_relations, :dependent => :destroy
-  has_many :users, :through => :user_school_relations
+  has_many :users, :as => :members, :class_name => 'User', :through => :user_school_relations
 
   accepts_nested_attributes_for :contact_info
 end
