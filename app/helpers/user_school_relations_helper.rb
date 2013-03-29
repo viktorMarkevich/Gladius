b4bd689794d7483559e3e00317e02daf32c8585f
markevich <viktor.markevich@faceit.com.ua>
2013-03-29 17:25:07 +0200

    cleanup

--- app/helpers/user_school_relations_helper.rb
+++
@@ -1,8 +0,0 @@
-module UserSchoolRelationsHelper
-
-  def user_age(birthday)
-    now = Time.now.utc.to_date
-    now.year - birthday.year - ((now.month > birthday.month ||
-        (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
-  end
-end
