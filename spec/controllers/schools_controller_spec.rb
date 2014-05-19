7b6a0a8be096e96d80acc5770408d3b6e4753b41
Viktor Markevich <viktor.markevich@faceit.com.ua>
2014-05-19 15:40:42 +0300

    added test for #new method school controller

--- spec/controllers/schools_controller_spec.rb
+++ spec/controllers/schools_controller_spec.rb
@@ -51,52 +51,16 @@ describe SchoolsController do
         response.should render_template :show
       end
     end
-    #
-    # context 'GET #edit' do
-    #   it 'assigns the requested user to @user' do
-    #     get :edit, id: @user
-    #     assigns(:user).should eq(@user)
-    #   end
-    #
-    #   it 'renders the #edit view' do
-    #     get :edit, id: FactoryGirl.create(:user)
-    #     response.should render_template :edit
-    #   end
-    # end
-    #
-    # describe 'PUT update' do
-    #   context 'valid attributes' do
-    #     it 'located the requested @user' do
-    #       put :update, id: @user, user: FactoryGirl.attributes_for(:user)
-    #       assigns(:user).should eq(@user)
-    #     end
-    #     it 'changes @users attributes' do
-    #       put :update, id: @user, user: FactoryGirl.attributes_for(:user, first_name: 'Larry', last_name: 'Smith')
-    #       @user.reload
-    #       @user.first_name.should eq('Larry')
-    #       @user.last_name.should eq('Smith')
-    #     end
-    #     it 'redirects to the updated user' do
-    #       put :update, id: @user, user: FactoryGirl.attributes_for(:user)
-    #       response.should redirect_to @user
-    #     end
-    #
-    #     it 'redirects to the updated user' do
-    #       put :update, id: @user.id, user: FactoryGirl.attributes_for(:invalid_user)
-    #       response.should render_template(:edit)
-    #     end
-    #   end
-    #
-    #   context 'invalid attributes' do
-    #     it 'locates the requested @user' do
-    #       put :update, id: @user, user: FactoryGirl.attributes_for(:invalid_user)
-    #       assigns(:user).should eq(@user)
-    #     end
-    #     it 'does not change @users attributes' do
-    #       put :update, id: @user, user: FactoryGirl.attributes_for(:user, first_name: 'Larry', last_name: 'Smith')
-    #       @user.reload @user.first_name.should_not eq('Larry')
-    #       @user.last_name.should eq('Smith')
-    #     end
-    #   end
-    # end
+
+      it "assigns form params[:school] to the new school" do
+        @school = FactoryGirl.build :school
+        get :new, user_id: @user.id
+        assigns(:school).should_not be_nil
+      end
+
+      it "renders the #new view" do
+        get :new, user_id: @user.id
+        response.should render_template :new
+      end
+
 end
