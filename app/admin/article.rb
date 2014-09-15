f754f7f342d08cb3992f8d9d37cda4c7b5de6812
Viktor Markevich <viktor.markevich@faceit.com.ua>
2014-09-15 16:30:33 +0300

    customised AA index page

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -1,5 +1,13 @@
 ActiveAdmin.register Article do
 
+  filter :title
+  filter :body
+  filter :published
+  filter :to_homepage
+  filter :created_at
+  filter :kind, as: :select, :collection => Article::ARTICLE_TYPES
+  filter :admin_author, collection: AdminUser.all.map{ |a| [a.email, a.id] }, label: 'Author'
+
   before_create do |product|
     product.admin_author = current_admin_user
   end
@@ -9,16 +17,23 @@ ActiveAdmin.register Article do
     params
   end
 
-  # index do
-  #   column :author_id do |article|
-  #     AdminUser.find(article.author_id).email
-  #   end
-  # end
+  index do
+    selectable_column
+    column :id
+    column :title
+    column :body
+    column :published
+    column :to_homepage
+    column :created_at
+    column :updated_at
+    column :kind
+    column 'Author', :admin_author, :sortable => true
+  end
+
 
   form do |f|
     f.inputs do
       f.input :school_id, as: :select, collection: School.all.map{ |s| [ s.name, s.id ] }
-      # f.input :author_id, as: :select, collection: User.all.map{ |s| [ s.full_name, s.id ] }
       f.input :title
       f.input :body
       f.input :kind, :label => 'Type article', as: :select, :collection => Article::ARTICLE_TYPES
